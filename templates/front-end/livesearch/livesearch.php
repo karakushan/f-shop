e3311c8d9ecc1fbbf8142a80252ca2d0438ada42
Vitaliy Karakushan <karakushan@gmail.com>
2018-05-09 17:09:44 +0300

    удалил livesearch шаблон

--- templates/front-end/livesearch/livesearch.php
+++
@@ -1,8 +0,0 @@
-<div class="livesearch-item">
-	<a href="<?php the_permalink() ?>">
-		<?php if (has_post_thumbnail()) the_post_thumbnail() ?>
-		<span class="title"><?php the_title() ?></span> <br>
-
-	</a>
-	<p class="price"><?php do_action('fs_the_price') ?></p>
-</div>
