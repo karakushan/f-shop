1cca7d5dd44760bc8b37d217ea974b45ac143cd1
Vitaliy Karakushan <karakushan@gmail.com>
2019-03-08 14:48:31 +0200

    пофиксил метод get_title()

--- lib/FS_Product_Class.php
+++ lib/FS_Product_Class.php
@@ -251,12 +251,17 @@ class FS_Product_Class {
 	 *
 	 * @param int $product_id
 	 *
+	 * @param null $variation_id
+	 *
 	 * @return string
 	 */
-	function get_title( $product_id = 0 ) {
-		$product_id = $product_id ? $product_id : $this->id;
+	function get_title( $product_id = 0, $variation_id = null ) {
+		$product_id   = $product_id ? $product_id : $this->id;
+		$variation_id = ! is_null( $variation_id ) && is_numeric( $variation_id ) ? $variation_id : $this->variation;
+		$variation    = $this->get_variation( $product_id, $variation_id );
+		$title        = ! empty( $variation['name'] ) ? $variation['name'] : get_the_title( $product_id );
 
-		return get_the_title( $product_id );
+		return $title;
 	}
 
 	/**
