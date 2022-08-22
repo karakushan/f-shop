c98bf63ca270a38fa12928b67fbd042f4cc120ed
Vitaliy Karakushan <karakushan@gmail.com>
2022-08-22 23:09:44 +0300

    fix hreflang for wpglobus

--- lib/integrations/WP_Globus.php
+++ lib/integrations/WP_Globus.php
@@ -12,43 +12,8 @@ class WP_Globus {
 	public $switcher_name = '';
 
 	public function __construct() {
-		add_filter( 'wpglobus_hreflang_tag', [ $this, 'wpglobus_hreflang_tag_filter' ], 10, 1 );
 		add_action( 'fs_wpglobus_language_switcher', [ $this, 'wpglobus_language_switcher' ] );
 		add_action( 'wp_footer', [ $this, 'footer_inline_scripts' ] );
-	}
-
-	/**
-	 * Заменяет hreflang в head
-	 *
-	 * @param $hreflangs
-	 *
-	 * @return mixed
-	 */
-	function wpglobus_hreflang_tag_filter( $hreflangs ) {
-		$product_taxonomy_name = FS_Config::get_data( 'product_taxonomy' );
-		$post_type             = FS_Config::get_data( 'post_type' );
-        
-
-		// Меняем hreflang для категорий товара
-		if ( is_archive( $post_type ) && is_tax( $product_taxonomy_name ) ) {
-			$queried_object = get_queried_object();
-			$slug           = get_term_meta( $queried_object->term_id, '_seo_slug__uk', 1 )
-				? get_term_meta( $queried_object->term_id, '_seo_slug__uk', 1 ) : $queried_object->slug;
-			if ( fs_option( 'fs_disable_taxonomy_slug' ) ) {
-				$hreflangs['ru'] = sprintf( '<link rel="alternate" hreflang="ru" href="%s"/>', site_url( $queried_object->slug . '/' ) );
-				$hreflangs['ua'] = sprintf( '<link rel="alternate" hreflang="uk" href="%s"/>', site_url( 'ua/' . $slug . '/' ) );
-			} else {
-				$hreflangs['ru'] = sprintf( '<link rel="alternate" hreflang="ru" href="%s"/>', site_url( $product_taxonomy_name . '/' . $queried_object->slug . '/' ) );
-				$hreflangs['ua'] = sprintf( '<link rel="alternate" hreflang="uk" href="%s"/>', site_url( 'ua/' . $product_taxonomy_name . '/' . $slug . '/' ) );
-			}
-		} elseif ( is_singular( $post_type ) ) {
-			global $post;
-			$slug_ua         = get_post_meta( $post->ID, 'fs_seo_slug__uk', 1 );
-			$hreflangs['ua'] = sprintf( '<link rel="alternate" hreflang="uk" href="%s"/>', $slug_ua ? site_url( sprintf( '%s/%s/%s/', 'ua', $post_type, $slug_ua ) )
-				: WPGlobus_Utils::localize_current_url( 'ua' ) );
-			$hreflangs['ru'] = sprintf( '<link rel="alternate" hreflang="ru" href="%s"/>', WPGlobus_Utils::localize_current_url( 'ru' ) );
-		}
-		return $hreflangs;
 	}
 
 	/**
