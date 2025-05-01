f309564620c49d7d6e09bd627fe0e98327311e65
Vitaliy <karakushan@gmail.com>
2025-05-01 11:07:09 +0300

    Refactor product gallery template: improved structure for thumbnail and main image displays, updated Swiper integration for better functionality, and enhanced code readability.

--- templates/front-end/product/gallery.php
+++ templates/front-end/product/gallery.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var array $gallery_images_ids An array of image IDs for the gallery.
  * @var array $args Additional arguments for the gallery configuration.
@@ -6,33 +7,37 @@
 ?>
 <div class="fs-product-gallery">
     <div class="fs-product-gallery__thumbs">
-        <div class="swiper swiper-container" id="productGalleryThumbs">
+        <div class="swiper" id="<?php echo $args['thumbs_gallery_id']; ?>">
             <div class="swiper-wrapper">
-				<?php foreach ( $gallery_images_ids as $image_id ) : ?>
-                    <div class="fs-product-gallery__thumb swiper-slide">
-						<?php echo wp_get_attachment_image( $image_id, 'thumbnail' ); ?>
+                <?php foreach ($gallery_images_ids as $image_id) : ?>
+                    <div class="swiper-slide">
+                        <div class="fs-product-gallery__thumb ">
+                            <?php echo wp_get_attachment_image($image_id, 'thumbnail'); ?>
+                        </div>
                     </div>
-				<?php endforeach; ?>
+                <?php endforeach; ?>
             </div>
         </div>
     </div>
 
     <div class="fs-product-gallery__main">
-        <div class="swiper swiper-container" id="productGallery">
+        <div class="swiper" id="<?php echo $args['big_gallery_id']; ?>">
             <div class="swiper-wrapper">
-				<?php foreach ( $gallery_images_ids as $image_id ) : ?>
-                    <div class="fs-product-gallery__main-slide swiper-slide">
-                        <a data-fslightbox="product-gallery"
-                           href="<?php echo esc_url( wp_get_attachment_image_url( $image_id, 'full' ) ) ?>">
-							<?php echo wp_get_attachment_image( $image_id, 'large' ); ?>
-                        </a>
+                <?php foreach ($gallery_images_ids as $image_id) : ?>
+                    <div class="swiper-slide">
+                        <div class="fs-product-gallery__main-slide">
+                            <a data-fslightbox="product-gallery"
+                                href="<?php echo esc_url(wp_get_attachment_image_url($image_id, 'full')) ?>">
+                                <?php echo wp_get_attachment_image($image_id, 'large'); ?>
+                            </a>
+                        </div>
                     </div>
-				<?php endforeach; ?>
+                <?php endforeach; ?>
             </div>
             <!-- navigation buttons -->
-			<?php echo $args['nextHtml'] ?: '' ?>
-			<?php echo $args['prevHtml'] ?: '' ?>
-			<?php echo $args['paginationHtml'] ?: '' ?>
+            <?php echo $args['nextHtml'] ?: '' ?>
+            <?php echo $args['prevHtml'] ?: '' ?>
+            <?php echo $args['paginationHtml'] ?: '' ?>
         </div>
     </div>
 </div>
